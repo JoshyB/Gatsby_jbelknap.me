72ae34c2e0edfe4c76c0d43f832576b7340ee3b7
Joshua B <seejoshcode@gmail.com>
2019-10-08 21:35:39 -0400

    added some placeholder images

--- src/components/templates/project_page_template.js
+++ src/components/templates/project_page_template.js
@@ -7,6 +7,8 @@ import Contact from "../contact"
 
 //images
 import externalLink from "../../images/external_link_icon.svg"
+import rcollins from "../../images/rcollins/rcollins_home.png"
+import rcollinsMobile from "../../images/rcollins/rcollins_home_mobile.png"
 
 //media query breakpoints
 import { device } from "../../utils/breakpoints"
@@ -15,7 +17,7 @@ const ProjectPageWrapper = styled.section`
   padding: 30px;
   display: grid;
   grid-template-columns: 1fr;
-  grid-template-rows: repeat(2, auto);
+  grid-template-rows: repeat(4, auto);
 
   .project_nav {
     display: flex;
@@ -44,6 +46,15 @@ const ProjectPageWrapper = styled.section`
     }
   }
 
+  .project_images {
+    display: flex;
+    height: 500px;
+    img {
+      height: 100%;
+      margin: 5px;
+    }
+  }
+
   @media ${device.tablet} {
     .project_title {
       h1 {
@@ -66,6 +77,10 @@ export default ({ data }) => {
         <a href={project.websiteURL}>
           <img src={externalLink} />
         </a>
+      </div>
+      <div className="project_images">
+        <img src={rcollins} alt="" />
+        <img src={rcollinsMobile} alt="" />
       </div>
       <p>{project.description}</p>
       <p>{project.about}</p>
